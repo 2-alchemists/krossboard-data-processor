87ff8a1fbf9635a8bcb3199b2d92da53a9e2b7d1
rchakode <rodrigue.chakode@gmail.com>
2019-11-11 22:20:02 +0100

    updated mock gcp

--- cluster-manager/mock_gcp.sh
+++ cluster-manager/mock_gcp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkddir -p ./computeMetadata/v1/project
-echo -n 10213780375 > computeMetadata/v1/project/numeric-project-id
+mkdir -p ./computeMetadata/v1/project
+echo -n 60213770375 > computeMetadata/v1/project/numeric-project-id
 
 python3 -m http.server
 
