ff3ddc4c696bf13ba09abedf456c8d470de933b3
rchakode <rodrigue.chakode@gmail.com>
2019-11-17 11:44:31 +0100

    refactoring moving functions to specific files
    refactoring moving functions to specific files

---
+++ cluster-manager/eks.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os/exec"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func updateEKSClusters() {
+	defer workers.Done()
+
+	updatePeriod := time.Duration(viper.GetInt64("koacm_update_interval")) * time.Minute
+	for {
+		awsRegion, err := getAWSRegion()
+		if err != nil {
+			log.WithError(err).Error("cannot retrieve AWS region")
+			time.Sleep(updatePeriod)
+			continue
+		}
+		svc := eks.New(session.New(), aws.NewConfig().WithRegion(awsRegion))
+		listInput := &eks.ListClustersInput{}
+		listResult, err := svc.ListClusters(listInput)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				log.WithError(err).Errorf("failed listing EKS clusters (%v)", aerr.Code())
+			} else {
+				log.WithError(err).Error("failed listing EKS clusters")
+			}
+			time.Sleep(updatePeriod)
+			continue
+		}
+		for _, clusterName := range listResult.Clusters {
+			cmdout, err := exec.Command(viper.GetString("koamc_awscli_command"),
+				"eks",
+				"update-kubeconfig",
+				"--name",
+				*clusterName,
+				"--region",
+				awsRegion).CombinedOutput()
+
+			if err != nil {
+				log.WithField("cluster", clusterName).Errorf("failed getting EKS cluster credentials: %v", string(cmdout))
+			} else {
+				log.WithField("cluster", clusterName).Debugln("added/updated  EKS cluster credentials")
+			}
+		}
+
+		time.Sleep(updatePeriod)
+	}
+}
+
+func getAWSRegion() (string, error) {
+	timeout := time.Duration(time.Second)
+	client := &http.Client{
+		Timeout: timeout,
+	}
+
+	req, err := http.NewRequest("GET",
+		viper.GetString("koamc_aws_metadata_service")+"/latest/meta-data/placement/availability-zone",
+		nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", errors.Wrap(err, "failed calling AWS metadata service")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "failed ready response from GCP metadata server")
+	}
+
+	bodyText := string(body)
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("AWS metadata service returned error: " + bodyText)
+	}
+
+	return bodyText[:len(bodyText)-1], nil
+}
--- cluster-manager/main.go
+++ cluster-manager/main.go
@@ -1,30 +1,18 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"os"
-	"os/exec"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/pkg/errors"
-
 	"bitbucket.org/koamc/kube-opex-analytics-mc/koainstance"
 	"bitbucket.org/koamc/kube-opex-analytics-mc/kubeconfig"
 	"bitbucket.org/koamc/kube-opex-analytics-mc/systemstatus"
-	gcontainerv1 "cloud.google.com/go/container/apiv1"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/eks"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	gcontainerpbv1 "google.golang.org/genproto/googleapis/container/v1"
 )
 
 var workers sync.WaitGroup
@@ -283,163 +271,11 @@ func orchestrateInstances(systemStatus *systemstatus.SystemStatus) {
 	}
 }
 
-func updateGKEClusters() {
-	defer workers.Done()
-
-	ctx := context.Background()
-	clusterManagerClient, err := gcontainerv1.NewClusterManagerClient(ctx)
-	if err != nil {
-		log.WithError(err).Errorln("failed to instanciate GKE cluster manager")
-		return
-	}
-
-	updatePeriod := time.Duration(viper.GetInt64("koacm_update_interval")) * time.Minute
-	for {
-		projectID, err := getGCPProjectID()
-		if projectID <= int64(0) {
-			log.WithError(err).Errorln("unable to retrieve GCP project ID")
-			time.Sleep(updatePeriod)
-			continue
-		}
-		listReq := &gcontainerpbv1.ListClustersRequest{
-			Parent: fmt.Sprintf("projects/%v/locations/-", projectID),
-		}
-		listResp, err := clusterManagerClient.ListClusters(ctx, listReq)
-		if err != nil {
-			log.WithError(err).Errorln("Failed to list GKE clusters")
-			time.Sleep(updatePeriod)
-			continue
-		}
-
-		for _, cluster := range listResp.Clusters {
-			cmdout, err := exec.Command(viper.GetString("koamc_gcloud_command"),
-				"container",
-				"clusters",
-				"get-credentials",
-				cluster.Name,
-				"--zone",
-				cluster.Zone).CombinedOutput()
-
-			if err != nil {
-				log.WithField("cluster", cluster.Name).Errorln("failed getting GKE cluster credentials:", string(cmdout))
-			} else {
-				log.WithField("cluster", cluster.Name).Debugln("added/updated GKE cluster credentials")
-			}
-		}
-
-		time.Sleep(updatePeriod)
-	}
-}
-
-func updateEKSClusters() {
-	defer workers.Done()
-
-	updatePeriod := time.Duration(viper.GetInt64("koacm_update_interval")) * time.Minute
-	for {
-		awsRegion, err := getAWSRegion()
-		if err != nil {
-			log.WithError(err).Error("cannot retrieve AWS region")
-			time.Sleep(updatePeriod)
-			continue
-		}
-		svc := eks.New(session.New(), aws.NewConfig().WithRegion(awsRegion))
-		listInput := &eks.ListClustersInput{}
-		listResult, err := svc.ListClusters(listInput)
-		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				log.WithError(err).Errorf("failed listing EKS clusters (%v)", aerr.Code())
-			} else {
-				log.WithError(err).Error("failed listing EKS clusters")
-			}
-			time.Sleep(updatePeriod)
-			continue
-		}
-		for _, clusterName := range listResult.Clusters {
-			cmdout, err := exec.Command(viper.GetString("koamc_awscli_command"),
-				"eks",
-				"update-kubeconfig",
-				"--name",
-				*clusterName,
-				"--region",
-				awsRegion).CombinedOutput()
-
-			if err != nil {
-				log.WithField("cluster", clusterName).Errorf("failed getting credentials for GKE cluster %v", string(cmdout))
-			} else {
-				log.WithField("cluster", clusterName).Debugln("added/updated credentials for GKE cluster in KUBECONFIG")
-			}
-		}
-
-		time.Sleep(updatePeriod)
-	}
-}
-
 func createDirIfNotExists(path string) error {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return os.MkdirAll(path, 0755)
 	}
 	return nil
-}
-
-func getAWSRegion() (string, error) {
-	timeout := time.Duration(time.Second)
-	client := &http.Client{
-		Timeout: timeout,
-	}
-
-	req, err := http.NewRequest("GET",
-		viper.GetString("koamc_aws_metadata_service")+"/latest/meta-data/placement/availability-zone",
-		nil)
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", errors.Wrap(err, "failed calling AWS metadata service")
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", errors.Wrap(err, "failed ready response from GCP metadata server")
-	}
-
-	bodyText := string(body)
-	if resp.StatusCode != http.StatusOK {
-		return "", errors.New("AWS metadata service returned error: " + bodyText)
-	}
-
-	return bodyText[:len(bodyText)-1], nil
-}
-
-func getGCPProjectID() (int64, error) {
-	timeout := time.Duration(time.Second)
-	client := &http.Client{
-		Timeout: timeout,
-	}
-
-	req, err := http.NewRequest("GET",
-		viper.GetString("koamc_gcp_metadata_service")+"/computeMetadata/v1/project/numeric-project-id",
-		nil)
-	req.Header.Add("Metadata-Flavor", "Google")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return viper.GetInt64("google_project_id"), errors.Wrap(err, "failed calling GCP metadata service")
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return -1, errors.Wrap(err, "failed ready response from GCP metadata service")
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return -1, errors.New("GCP metadata server returned error: " + string(body))
-	}
-	projectID, err := strconv.ParseInt(string(body), 10, 64)
-	if err != nil {
-		return -1, errors.Wrap(err, "unexpected non integer for project id")
-	}
-
-	return projectID, nil
 }
 
 func getCloudProvider() string {
