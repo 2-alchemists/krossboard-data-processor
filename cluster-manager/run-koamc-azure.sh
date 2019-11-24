db15d20fb3094f1ae938eff47a2ef81f349b37e2
rchakode <rodrigue.chakode@gmail.com>
2019-11-24 09:30:15 +0100

    udpated azure dev script

--- cluster-manager/run-koamc-azure.sh
+++ cluster-manager/run-koamc-azure.sh
@@ -1,3 +1,10 @@
-export KOAMC_LOG_LEVEL=info
-export KOAMC_AZURE_METADATA_SERVICE=http://127.0.0.1:8000
+export KOAMC_LOG_LEVEL="info"
+export KOAMC_AZURE_METADATA_SERVICE="http://127.0.0.1:8000"
+export KOAMC_AKS_USER="koamc-aks-user@account.onmicrosoft.com"
+export KOAMC_AZURE_KEYVAULT_NAME="koamc-cluster-manager"
+export KOAMC_AZURE_KEYVAULT_AKS_PASSWORD_VERSION="5bf6a32f47ca44c5a6b6bb9cb02ebbbc"
+export AZURE_TENANT_ID="1a0afef5-8b48-4ef0-97e7-864a90cd01ed"
+export AZURE_CLIENT_ID="866c3ed4-bf9d-4742-8a96-f1f9be1e7708"
+export AZURE_CLIENT_SECRET="nPhqC7WJMh0?d=E8rc7=lur9qITZmwI-"
+
 make run
