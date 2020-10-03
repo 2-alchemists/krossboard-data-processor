cc1d5bbe6f3894de082d44321047b91fb37a24a0
Chris Camel <camel.christophe@gmail.com>
2020-10-03 10:31:20 +0200

    test: improve test to support multiple cases

--- cmd/cluster_usage_test.go
+++ cmd/cluster_usage_test.go
@@ -51,55 +51,82 @@ func TestUsageDb(t *testing.T) {
 				start := time.Unix(int64(info["last_update"].(uint)), 0)
 				So(start.Unix(), ShouldEqual, now().Unix())
 
-				Convey("Given some values added in the instance of RRD", func() {
-					data := []struct {
-						t        int
-						cpuUsage float64
-						memUsage float64
-					}{
-						{t: 1, cpuUsage: 10, memUsage: 15},
-						{t: 2, cpuUsage: 20, memUsage: 25},
-						{t: 3, cpuUsage: 30, memUsage: 35},
-					}
-
-					for _, datum := range data {
-						err := usageDb.UpdateRRD(start.Add(time.Duration(5*datum.t)*time.Minute), datum.cpuUsage, datum.memUsage)
-
-						So(err, ShouldBeNil)
-					}
-
-					end := start.Add(time.Duration(5*3)*time.Minute)
-					Convey(fmt.Sprintf("When fetching usage for interval %s - %s", start, end), func() {
-						usage, err := usageDb.FetchUsageHourly(start, end)
-
-						So(err, ShouldBeNil)
-
-						Convey("Then average values retrieved are the ones expected", func() {
-							So(usage, ShouldResemble, &UsageHistory{
-								CPUUsage: []*UsageHistoryItem{
-									{
-										DateUTC: RoundTime(start.Add(time.Duration(5*2)*time.Minute), time.Duration(usageDb.Step)*time.Second),
-										Value:   10.066666666666666,
-									},
-									{
-										DateUTC: RoundTime(start.Add(time.Duration(5*3)*time.Minute), time.Duration(usageDb.Step)*time.Second),
-										Value:   20.066666666666666,
-									},
+				type data struct {
+					t        int
+					cpuUsage float64
+					memUsage float64
+				}
+				type input struct {
+					fetcher func(u *UsageDb, startTimeUTC time.Time, endTimeUTC time.Time) (*UsageHistory, error)
+					period  time.Duration
+					data    func() []data
+				}
+				tests := []struct {
+					name  string
+					input input
+					want  *UsageHistory
+				}{
+					{
+						name: "hourly test case - nominal",
+						input: input{
+							fetcher: (*UsageDb).FetchUsageHourly,
+							period:  time.Duration(15) * time.Minute,
+							data: func() []data {
+								return []data{
+									{t: 5, cpuUsage: 10, memUsage: 15},
+									{t: 10, cpuUsage: 20, memUsage: 25},
+									{t: 15, cpuUsage: 30, memUsage: 35},
+								}
+							},
+						},
+						want: &UsageHistory{
+							CPUUsage: []*UsageHistoryItem{
+								{
+									DateUTC: RoundTime(start.Add(time.Duration(5*2)*time.Minute), time.Duration(usageDb.Step)*time.Second),
+									Value:   10.066666666666666,
 								},
-								MEMUsage: []*UsageHistoryItem{
-									{
-										DateUTC: RoundTime(start.Add(time.Duration(5*2)*time.Minute), time.Duration(usageDb.Step)*time.Second),
-										Value:   15.066666666666666,
-									},
-									{
-										DateUTC: RoundTime(start.Add(time.Duration(5*3)*time.Minute), time.Duration(usageDb.Step)*time.Second),
-										Value:   25.066666666666666,
-									},
+								{
+									DateUTC: RoundTime(start.Add(time.Duration(5*3)*time.Minute), time.Duration(usageDb.Step)*time.Second),
+									Value:   20.066666666666666,
 								},
+							},
+							MEMUsage: []*UsageHistoryItem{
+								{
+									DateUTC: RoundTime(start.Add(time.Duration(5*2)*time.Minute), time.Duration(usageDb.Step)*time.Second),
+									Value:   15.066666666666666,
+								},
+								{
+									DateUTC: RoundTime(start.Add(time.Duration(5*3)*time.Minute), time.Duration(usageDb.Step)*time.Second),
+									Value:   25.066666666666666,
+								},
+							},
+						},
+					},
+				}
+
+				for _, test := range tests {
+					Convey(fmt.Sprintf("Given the test case '%s'", test.name), func() {
+						Convey("Given some values added in the instance of RRD", func() {
+							data := test.input.data()
+							for _, datum := range data {
+								err := usageDb.UpdateRRD(start.Add(time.Duration(datum.t)*time.Minute), datum.cpuUsage, datum.memUsage)
+
+								So(err, ShouldBeNil)
+							}
+
+							end := start.Add(test.input.period)
+							Convey(fmt.Sprintf("When fetching usage for interval %s - %s (%s)", start, end, test.input.period), func() {
+								usage, err := usageDb.FetchUsageHourly(start, end)
+
+								So(err, ShouldBeNil)
+
+								Convey("Then average values retrieved are the ones expected", func() {
+									So(usage, ShouldResemble, test.want)
+								})
 							})
 						})
 					})
-				})
+				}
 			})
 		})
 
